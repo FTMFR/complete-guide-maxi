f494d4811043dad7524c2866ed1fc72b8c88b51d
FTMFR <fatemefarajzade018@gmail.com>
2023-09-06 21:04:52 +0330

    start create app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const ExpenseItem = () => {
+    return (
+        <div>
+            <h2>Expense Item Here!</h2>
+        </div>
+    );
+};
+
+
+
+export default ExpenseItem;
